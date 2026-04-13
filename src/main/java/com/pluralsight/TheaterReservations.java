9ad34479c6279b4910d73d9f2459eb8f4833bab2
RickelviC <Cruzrickelvi@gmail.com>
2026-04-13 15:46:43 -0400

    added user input

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class TheaterReservations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("What date will you be coming (MM/dd/yyyy): ");
+        String date = scanner.nextLine();
+
+        System.out.print("How many tickets would you like? ");
+        int tickets = scanner.nextInt();
+
+        String result = tickets + " reserved for " + date + " under " + name;
+
+        System.out.println(result);
+
+    }
 }
