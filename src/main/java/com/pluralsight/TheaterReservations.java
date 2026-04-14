09a26b5e2ce420a1d71697d77c934c0ee6708285
RickelviC <Cruzrickelvi@gmail.com>
2026-04-14 08:35:23 -0400

    added method getName and getTicket

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -8,19 +8,10 @@ public class TheaterReservations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String tick = "";
-
         System.out.print("Please enter your name: ");
         String name = scanner.nextLine().trim();
 
-        int space = name.indexOf(" ");
-
-        String firstName = name.substring(0,space);
-
-        String lastName = name.substring(space + 1);
-
-        name = lastName + ", " + firstName;
-
+        name = getName(name);
 
         System.out.print("What date will you be coming (MM/dd/yyyy): ");
         String date = scanner.nextLine();
@@ -31,13 +22,29 @@ public class TheaterReservations {
         System.out.print("How many tickets would you like? ");
         int ticket = scanner.nextInt();
 
+        String tick = getTicket(ticket);
+
+        String result = tick + " reserved for " + reserved + " under " + name;
+        System.out.println(result);
+    }
+
+    public static String getName(String name){
+        int space = name.indexOf(" ");
+
+        String firstName = name.substring(0,space);
+
+        String lastName = name.substring(space + 1);
+
+        name = lastName + ", " + firstName;
+        return name;
+    }
+    public static String getTicket(int ticket){
+        String tick = "";
         if (ticket == 1){
             tick = (ticket) + " ticket";
         }else {
             tick = (ticket) + " tickets";
         }
-
-        String result = tick + " reserved for " + reserved + " under " + name;
-        System.out.println(result);
+        return tick;
     }
 }
